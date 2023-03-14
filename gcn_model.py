7ea58e100c70f3343d3a9511ebb94453b62cd6a1
Diego Torres <diegotg2000@hotmail.com>
2023-03-14 11:32:45 +0100

    train script and model mods

--- gcn_model.py
+++ gcn_model.py
@@ -1,10 +1,57 @@
-from typing import List
+from typing import List, Dict
 import torch
 from torch.nn import Linear, Parameter
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
 import torch.nn.functional as F
 
+
+class GCNNet(torch.nn.Module):
+    def __init__(self, model_params: Dict):
+        super().__init__()
+        self.gcn = GCN(**model_params['GCN'])
+        self.mlp = MLP(**model_params['MLP'])
+
+    def forward(self, x, edge_index, batch):
+        x = self.gcn(x, edge_index)
+        x = scatter(batch, x)
+        x = self.mlp(x)
+        return x
+    
+
+class GCN(torch.nn.Module):
+    def __init__(self, channels: List[int], activation=None) -> None:
+        super().__init__()
+        self.convs = torch.nn.ModuleList(GCNConv(channels[i], channels[i + 1]) for i in range(len(channels) - 1))
+        self.activation = activation
+
+    def forward(self, x, edge_index):
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
+            if i < len(self.convs) - 1:
+                if self.activation:
+                    x = self.activation(x)
+                else:
+                    x = F.relu(x)
+        return x
+
+
+class MLP(torch.nn.Module):
+    def __init__(self, channels: List[int], activation=None) -> None:
+        super().__init__()
+        self.layers = torch.nn.ModuleList(Linear(channels[i], channels[i + 1]) for i in range(len(channels) - 1))
+        self.activation = activation
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if i < len(self.layers) - 1:
+                if self.activation:
+                    x = self.activation(x)
+                else:
+                    x = F.relu(x)
+        return x
+    
 
 class GCNConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
@@ -49,41 +96,14 @@ class GCNConv(MessagePassing):
         # Step 4: Normalize node features.
         return norm.view(-1, 1) * x_j
 
-    
-class GCN(torch.nn.Module):
-
-    def __init__(self, channels: List[int], activation=None) -> None:
-        super().__init__()
-        self.convs = torch.nn.ModuleList(GCNConv(channels[i], channels[i + 1]) for i in range(len(channels) - 1))
-        self.activation = activation
-
-    def forward(self, x, edge_index):
-        for i, conv in enumerate(self.convs):
-            x = conv(x, edge_index)
-            if i < len(self.convs) - 1:
-                if self.activation:
-                    x = self.activation(x)
-                else:
-                    x = F.relu(x)
-        return x
-
         
-class GCNNet(torch.nn.Module):
-    def __init__(self, channels: List[int]):
-        super().__init__()
-        self.gcn = GCN(channels)
-        self.lin1 = torch.nn.Linear(channels[-1], 8)
-        self.lin2 = torch.nn.Linear(8, 1)
+def scatter(batch, x):
+    y = torch.zeros(torch.max(batch)+1, x.size(1), dtype=x.dtype, device=x.device)
+    for node, graph_node in enumerate(batch):
+        y[graph_node, :] += x[node, :]
+    _, counts = torch.unique_consecutive(batch, return_counts=True)
+    y = y/counts.unsqueeze(1)
+    return y
 
-    def forward(self, x, edge_index, batch):
-        x = self.gcn(x, edge_index)
-        y = torch.zeros(torch.max(batch)+1, x.size(1), dtype=x.dtype, device=x.device)
-        for node, graph_node in enumerate(batch):
-            y[graph_node, :] += x[node, :]
-        _, counts = torch.unique_consecutive(batch, return_counts=True)
-        y = y/counts.unsqueeze(1)
-        y = self.lin1(y)
-        y = F.relu(y)
-        y = self.lin2(y)
-        return y
+
 
---
+++ train_gcn.py
@@ -0,0 +1,142 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric as pyg
+from sklearn.metrics import r2_score
+from torch.nn.functional import one_hot
+from torch_geometric.datasets import ZINC
+from torch_geometric.loader import DataLoader
+from torch_geometric.utils import to_networkx
+
+from gcn_model import GCN, GCNNet
+
+
+def train(model, train_loader, optimizer, loss):
+    model.train()
+    loss_acc = 0
+    total_graphs = 0
+    total_preds = []
+    labels = []
+
+    for graph_batch in train_loader:
+        graph_batch = graph_batch.to(device)
+        optimizer.zero_grad()
+        
+        x_oh = one_hot(graph_batch.x.flatten(), num_classes=21).type(torch.cuda.FloatTensor)
+        preds = model(x_oh, graph_batch.edge_index, graph_batch.batch).squeeze()
+        loss_val = loss(preds, graph_batch.y)
+        loss_acc += loss_val.item()
+        total_graphs += graph_batch.num_graphs
+        loss_val.backward()
+        optimizer.step()
+
+        total_preds.extend(preds.cpu().detach().numpy())
+        labels.extend(graph_batch.y.cpu().detach().numpy())
+        
+    loss_acc /= total_graphs
+    r2 = r2_score(labels, total_preds)
+    return loss_acc, r2
+
+
+def validate(model, valid_loader, loss):
+    model.eval()
+    loss_acc = 0
+    total_graphs = 0
+    total_preds = []
+    labels = []
+    with torch.no_grad():
+        for graph_batch in valid_loader:
+            graph_batch = graph_batch.to(device)
+            x_oh = one_hot(graph_batch.x.flatten(), num_classes=21).type(torch.cuda.FloatTensor)
+            preds = model(x_oh, graph_batch.edge_index, graph_batch.batch).squeeze()
+            loss_val = loss(preds, graph_batch.y)
+            loss_acc += loss_val.item()
+            total_graphs += graph_batch.num_graphs
+            total_preds.extend(preds.cpu().numpy())
+            labels.extend(graph_batch.y.cpu().numpy())
+
+    r2 = r2_score(labels, total_preds)            
+    loss_acc /= total_graphs
+    return loss_acc, r2
+
+
+if __name__ == '__main__':
+    import json
+
+    DATA_FOLDER = './data/zinc/'
+    NUMBER_OF_ATOMS = 21
+
+    MODEL_PARAMS = {
+        'GCN': {
+            'channels': [
+                NUMBER_OF_ATOMS,
+                16,
+                16
+            ]
+        },
+        'MLP': {
+            'channels': [
+                16,
+                10,
+                1
+            ]
+        },
+        'learning_rate': 0.0001,
+    }
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    train_ds = ZINC(root=DATA_FOLDER, subset=True, split='train')
+    val_ds = ZINC(root=DATA_FOLDER, subset=True, split='val')
+
+
+    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
+    test_loader = DataLoader(val_ds, batch_size=32, shuffle=False)
+
+
+    model = GCNNet(MODEL_PARAMS).to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=MODEL_PARAMS['learning_rate'])
+    loss = nn.MSELoss()
+
+    train_loss = []
+    test_loss = []
+    train_r2 = []
+    test_r2 = []
+
+    for epoch in range(3):
+        print('EPOCH:', epoch+1)
+        print('Training...')
+        loss_value, r2_value = train(model, train_loader, optimizer, loss)
+        train_loss.append(loss_value)
+        train_r2.append(r2_value)
+
+        print('Validating..')
+        loss_value, r2_value = validate(model, test_loader, loss)
+        test_loss.append(loss_value)
+        test_r2.append(r2_value)
+
+
+        print('Training Loss:', train_loss[-1])
+        print('Training R2:', train_r2[-1])
+        print('Validation Loss:', test_loss[-1])
+        print('Validation R2:', test_r2[-1])
+
+
+    RESULT_DICT = {
+        'params': MODEL_PARAMS,
+        'train_loss': train_loss,
+        'test_loss': test_loss,
+        'train_r2': train_r2,
+        'test_r2': test_r2
+    }
+
+    with open('results.json', 'w') as f:
+        json.dump(RESULT_DICT, f, indent=4)
+
+
